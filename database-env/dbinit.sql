23a583474261a0ed49995024913e0a6cc27d3795
1992clement <cltrumpff@gmail.com>
2019-04-05 19:28:32 +0800

    environment up and running

---
+++ database-env/dbinit.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE TNLdb;
+USE TNLdb;
+CREATE TABLE IF NOT EXISTS events (
+    event_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    place_name VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    start_time DATETIME NOT NULL,
+    event_url VARCHAR(255),
+    picture_url VARCHAR(255),
+    picture TEXT,
+    PRIMARY KEY (event_id)
+)  ENGINE=INNODB;
